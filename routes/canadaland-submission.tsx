4226bbc1db58ec46c90590734268e7dd554d0120
Tim Godin <tim_godin@msn.com>
2024-04-29 11:46:16 -0600

    test deploy

--- routes/canadaland-submission.tsx
+++ routes/canadaland-submission.tsx
@@ -3,6 +3,30 @@ export default function Home() {
     return (<div class="text-left">
         <h1><a href="https://www.canadaland.com/introducing-canadalabs/">CanadaLabs</a></h1>
 <p>Due May 31st. 3 minute audio local news.</p>
+<p>the rules:</p>
+<ol>
+<li>Be 3 minutes in length (give or take 10 seconds)</li>
+<li>Be rooted in place. Think local.</li>
+<li>Involve at least two of the following elements:</li>
+</ol>
+<ul>
+<li>a reference to local folklore or an urban myth</li>
+<li>tape from a local mainstay — e.g., a restaurant, park, city hall, neighbour’s yard</li>
+<li>the voice of someone you don’t yet know</li>
+<li>the sound of water (outdoor, indoor, whatever)</li>
+</ul>
+<p>requirements:</p>
+<ul>
+<li>Your full name and contact information</li>
+<li>An mp3 of your finished piece</li>
+<li>The piece’s title and the name of the place featured</li>
+<li>A short description of the work (100 words or less)</li>
+<li>An indication of which elements you incorporated into your piece</li>
+<li>An image related to your story</li>
+<li>A short bio (100 words or less) and headshot</li>
+<li>Links to websites / social handles</li>
+<li>An English-language transcript, if the submission is not in English or the audio is unclear</li>
+</ul>
 
 </div>);
 }
