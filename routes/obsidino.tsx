a6b88f25205b66a961169ced05ca3af219d1170a
Tim Godin <tim_godin@msn.com>
2024-04-29 23:15:44 -0600

    thoughts on project

--- routes/obsidino.tsx
+++ routes/obsidino.tsx
@@ -2,12 +2,21 @@ export default function Home() {
     
     return (<div class="main">
         <h1>Obsidino</h1>
-<p>Single executable that sits above an Obsidian Vault and deploys to Deno Deploy.</p>
+<p>Single executable that sits above an Obsidian Vault and, ideally with little to no config, turns Vault and images into a website via Deno Deploy.</p>
 <ul>
 <li><a href="https://obsidian.md/">Obsidian - Sharpen your thinking</a></li>
 <li><a href="https://deno.com/deploy">Deno Deploy | Deno</a></li>
 </ul>
-<p><a href="contact">Contact</a> me to contribute or sign up for launch notice.</p>
+<h2>notes in progress</h2>
+<ul>
+<li>1GB limit</li>
+</ul>
+<h2>current progress</h2>
+<p>I'm astonned that there aren't any plugins to do obsidian to html directly. Tried reMarked, MarkdownIt, Marked, a couple others, all with sub plugs and options... Makes a guy wanna get Co-Pilot back I've trudged through so much repetitiveness.</p>
+<p>The combination of GFM (github flavored markdown) and whatever Obsidian is calling it's internal linking scheme is tricky to deal with.</p>
+<p>Shouldn't be long before the kinks're worked out, but since my use case is so limited I feel like I'm missing a lot of weird cases.</p>
+<p>Probably best to sprint towards something working: turning 1 flat directory Obsidian Vault with internal links and images into a website in one or two clicks.</p>
+<p>Would be a better 'product' if it had support for canvas/graph view though.</p>
 
 </div>);
 }
