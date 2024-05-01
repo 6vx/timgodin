b91150197b4e334c25e8a75aac1db0409cb2eca0
Tim Godin <tim_godin@msn.com>
2024-05-01 11:43:24 -0600

    timface

--- routes/ai.tsx
+++ routes/ai.tsx
@@ -2,8 +2,8 @@ export default function Home() {
     
     return (<div class="main text-left w-max-md mx-auto">
         <h1>Artificial ...Intelligence?</h1>
-<p>None of the writing published on this website, <a href="https://timgod.in" title="https://timgod.in">timgod.in</a> , were written by an AI tool.</p>
-<p>I use different tools powered by large language models while brainstorming, researching and</p>
+<p>None of the writing published on this website, <a href="https://timgod.in" title="https://timgod.in">timgod.in</a>, was written by an AI tool.</p>
+<p>I use different tools powered by large language models while brainstorming and for making images.</p>
 <p>I do not intend to use it in any way that can cause harm to others.</p>
 <p>This is an /ai page. You can create one on your website too. If you do, <a href="https://docs.google.com/forms/d/e/1faipqlse6uqgjy8h4ugks6p0a3lk_ywoskcizzz93rnoh0pf8qycaca/viewform">consider listing it on this public database</a>.</p>
 
--- routes/index.tsx
+++ routes/index.tsx
@@ -2,8 +2,13 @@ export default function Home() {
     
     return (<div class="main text-left w-max-md mx-auto">
         <h1>Tim Godin</h1>
-<p><a href="now">now</a></p>
-<p><a href="ai">AI</a></p>
+<p>!<a href="timfacelowres.png">timfacelowres.png</a>
+Check out some of my ongoing projects or <a href="contact">Contact</a> me.</p>
+<ul>
+<li><a href="obsidino">Obsidino</a></li>
+<li><a href="now">now</a></li>
+<li><a href="ai">AI</a></li>
+</ul>
 
 </div>);
 }
