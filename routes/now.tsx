595864373e5123c41b6af2a3b20ec9aa584f6ddc
Tim Godin <tim_godin@msn.com>
2024-05-10 14:48:51 -0600

    sending up a content refresh with two dead links

--- routes/now.tsx
+++ routes/now.tsx
@@ -2,10 +2,14 @@ export default function Home() {
     
     return (<div class="main text-left w-max-md mx-auto">
         <h1>nownownow</h1>
-<p>I want to get out this Canadalabs thing. Seems like a good link to have.</p>
+<p>I want to get out this Canadalabs thing. Seems like a good link to have. Also working on my blog.</p>
+<p>Primary focus is getting a job in security. Would like to have a security company within two years.</p>
 <h2>processing</h2>
 <ul>
+<li><a href="obsidino">Obsidino</a></li>
 <li><a href="canadaland-submission">Canadaland Submission</a></li>
+<li><a href="security-guard-course">Security Guard Course</a></li>
+<li><a href="kettlebell-routine">Kettlebell Routine</a></li>
 </ul>
 
 </div>);
--- routes/obsidino.tsx
+++ routes/obsidino.tsx
@@ -2,23 +2,36 @@ export default function Home() {
     
     return (<div class="main text-left w-max-md mx-auto">
         <h1>Obsidino</h1>
-<p>Single executable that sits above an Obsidian Vault and, ideally with little to no config, turns Vault and images into a website via Deno Deploy.</p>
+<img src="obsidino.png" alt="obsidino.png" />
+<p>Vault -&gt; URL in one click.</p>
+<p>Update/publish by running it again.</p>
 <ul>
 <li><a href="https://obsidian.md/">Obsidian - Sharpen your thinking</a></li>
 <li><a href="https://deno.com/deploy">Deno Deploy | Deno</a></li>
 </ul>
-<h2>notes in progress</h2>
+<h2>why</h2>
+<p>Obsidian Publish 8$ USD / month
+Obsidino = 2$ USD / month</p>
+<p><a href="https://www.obsidian.md/publish">Obsidian's offering</a> is too expensive. We're talking about publishing some text here.</p>
+<p>Also I've found that most of the sites published with it don't even display in Edge. I'm not sure if that's a result of poor work on their end or some rogue extension on mine, but I also don't care. I can make it better myself.</p>
+<h2>expected limitations</h2>
 <ul>
-<li>1GB limit</li>
-<li><s>text only</s></li>
+<li>1GB maximum size</li>
+<li>images are downsized</li>
+<li>extra costs for domains and excessive traffic</li>
 </ul>
-<h2>current progress</h2>
-<p>Basic conversion functions
-Image tags parsed
-file write to tsx</p>
-<h2>in progress</h2>
-<p>parsing aliases regex
-internal non-obsidian links (skip/do not create file?)</p>
+<h2>progress</h2>
+<ul>
+<li>20240510 internal alias links working</li>
+<li>20240505 PNG files working</li>
+<li>20240502 internal links working</li>
+</ul>
+<h2>next steps</h2>
+<ul>
+<li>check non-internal alias links</li>
+<li>automatic downsize images for web (optional)</li>
+<li><em><strong>interpret frontmatter</strong></em></li>
+</ul>
 
 </div>);
 }
