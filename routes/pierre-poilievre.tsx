0bd7adf3e5603b9a47beb854c2aea8f28fa0f32c
Tim Godin <tim_godin@msn.com>
2024-05-10 15:53:53 -0600

    testing images

---
+++ routes/pierre-poilievre.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    
+    return (<div class="main text-left w-max-md mx-auto">
+        <h1>Pierre Poilievre</h1>
+<p>As I understand it this fella's been a politician his whole life and now is gaming the algorithm with divisiveness to rise to power.</p>
+<p>Sounds a bit sketch I gotta say.</p>
+<p>If I understood his positive vision and accompanying policy I may change my mind.</p>
+<p>But it seems like he just uses negativity to get views and... well damn it's <strong>workin</strong>' but that don't mean it's <strong>good</strong>.</p>
+
+</div>);
+}
