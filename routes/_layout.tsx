b096d6abff809dfb7cb8342c490ac5a23ce54138
Tim Godin <tim_godin@msn.com>
2024-05-11 16:35:09 -0600

    image styles

--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -4,6 +4,9 @@ export default function Layout({ Component, state }: PageProps) {
   // do something with state here
   return (
     <div class="layout flex-col">
+      <div className="menuButton">
+        <h4><a href="/">home</a></h4>
+      </div>
       <Component />
     </div>
   );
