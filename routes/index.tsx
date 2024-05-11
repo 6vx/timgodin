20725a894861be40f4435a0ebf4c2b9572f78402
Tim Godin <tim_godin@msn.com>
2024-05-10 18:12:40 -0600

    add face pic

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,6 +2,7 @@ export default function Home() {
     
     return (<div class="main text-left w-max-md mx-auto">
         <h1>Tim Godin</h1>
+<img src="timface.png" alt="timface.png" />
 <p>Widget artificer.</p>
 <p>You can <a href="contact">contact me</a> for more.</p>
 <ul>
