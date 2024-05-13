3e3aa47edff8e61099d88b850edeeca2c9626166
Tim Godin <tim_godin@msn.com>
2024-05-12 21:57:28 -0600

    lol

--- routes/maid-expansion.tsx
+++ routes/maid-expansion.tsx
@@ -3,21 +3,13 @@ export default function Home() {
     return (<div class="main text-left w-max-md mx-auto">
         <h1>MAID Expansion</h1>
 <h2>EXPAND MAID NOW</h2>
-<ol>
-<li>Expanded Eligibility</li>
-</ol>
+<h3>Expanded Eligibility</h3>
 <p>Eligibility requirements will be dropped and all consenting or unconscious participants registered and processed at the most expedient rate possible.</p>
-<ol start="2">
-<li>Separate From Healthcare</li>
-</ol>
+<h3>Separate From Healthcare</h3>
 <p>MAID will be made available outside of the healthcare system, which will no longer be serving people over the age of 65.</p>
-<ol start="3">
-<li>Automatic Enrollment</li>
-</ol>
+<h3>Automatic Enrollment</h3>
 <p>Repeat violent offenders, people over 65 and people who talk in theaters will be automatically enrolled in MAID.</p>
-<ol start="4">
-<li>Expanded Availability</li>
-</ol>
+<h3>Expanded Availability</h3>
 <p>Underground Railroad 2.0: International applications are approved without stipulation with immediate disposal available at</p>
 <ul>
 <li>YYZ</li>
