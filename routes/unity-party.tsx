75887937cc7556b50c654610a27d3ce969a7d7fb
Tim Godin <tim_godin@msn.com>
2024-05-13 23:16:31 -0600

    politics, u kno?

--- routes/unity-party.tsx
+++ routes/unity-party.tsx
@@ -1,13 +1,14 @@
 export default function Home() {
     
     return (<div class="main text-left w-max-md mx-auto">
-        <h1>One World Government</h1>
-<h2>Join the World</h2>
-<h3>combine North America</h3>
+        <h4>One Planet, One People, One Government</h4>
+<h2>Unite the World</h2>
+<p>Canada should lead the way in uniting earth under the hard earned values of the West by uniting the United States and Europe.</p>
+<h2>Combine North America</h2>
 <p>Join the US as the next 12 states (nine provinces and three territories, assuming enactment of the <a href="give-quebec-back">Give Quebec Back</a> plan).</p>
-<p>24 new Senators and many congresspeople will disrupt the makeup of all ; Canadians and Americans can cherry-pick the best of both cultures and then proceed in it's next aim.</p>
-<h2>consolidate power</h2>
-<p>10,000 years of peace will be administered.</p>
+<p>24 new Senators and many congresspeople will disrupt the makeup of Congress; Canadians and Americans can cherry-pick the best of both systems and cultures and then proceed in it's next aim.</p>
+<h2>Consolidate Power</h2>
+<p>10,000 years of peace will be administered to Earth while humans become an interplanetary species.</p>
 
 </div>);
 }
