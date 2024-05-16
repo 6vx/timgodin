e2cffd2d81c7151c9a6dd364dad2430b6dff8b33
Tim Godin <tim_godin@msn.com>
2024-05-16 14:39:48 -0600

    some small changes to the stuff here lol k bye

--- routes/61.tsx
+++ routes/61.tsx
@@ -5,24 +5,10 @@ export default function Home() {
     return (<div class="main text-left w-max-md mx-auto">
         
     <h1>Sarah</h1>
+<h2>무당</h2>
 <img src="61.png" alt="61.png" />
 <blockquote>
-<p>61 is the 18th prime number, twinned with 59</p>
-</blockquote>
-<blockquote>
-<p>centered square number, sum of 5^2 and 6^2.</p>
-</blockquote>
-<blockquote>
-<p>is the fourth Cuban Prime</p>
-</blockquote>
-<blockquote>
-<p>atomic number of <a target="_blank" href="https://en.wikipedia.org/wiki/promethium">Promethium</a></p>
-</blockquote>
-<blockquote>
-<p>dial 61 to australia</p>
-</blockquote>
-<blockquote>
-<p>points required to win cribbage</p>
+<p>focused creativity, turning visions into reality, inspired action</p>
 </blockquote>
 
 
