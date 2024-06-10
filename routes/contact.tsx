687bba1c392bf7a5323488f9ccc6d38a222b215c
Tim Godin <tim_godin@msn.com>
2024-06-10 09:10:00 -0600

    20240610: change email

--- routes/contact.tsx
+++ routes/contact.tsx
@@ -9,7 +9,7 @@ export const handler: Handlers = {
   },
 };
 const data = 
-{"color":"red","description":"Reach out to me about your project!","title":"Contact Information","tags":["email","linkedIn"],"created":"2024-04-26","updated":"2024-05-19","private":"false"}
+{"color":"red","description":"Reach out to me about your project!","title":"Contact Information","tags":["email","linkedIn"],"created":"2024-04-26","updated":"2024-06-10","private":"false"}
 export default function Home({ data }: PageProps) {
   return (
     <>
@@ -25,7 +25,7 @@ export default function Home({ data }: PageProps) {
 
 <h1>Contact</h1>
 <h2>email</h2>
-<p><a href="mailto:tim@timgod.in">tim@timgod.in</a></p>
+<p><a href="mailto:tim_godin@msn.com">tim_godin@msn.com</a></p>
 <h2>linkedIn</h2>
 <p><a target="_blank" href="https://www.linkedin.com/in/godini/">Tim Godin | LinkedIn</a></p>
 
