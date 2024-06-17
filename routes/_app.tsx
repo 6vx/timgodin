89ce3981d5634605ac362d03c2562b9da4c1d786
Tim Godin <tim_godin@msn.com>
2024-06-17 13:07:08 -0600

    20240617: add google analytics

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -10,6 +10,15 @@ export default function App({ Component }: PageProps) {
         <link rel="icon" href="/favicon.png" type="image/png"></link>
 
       </head>
+
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-X4B7FREDZL"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-X4B7FREDZL');
+      </script>
      
       <body>
         <Component />
