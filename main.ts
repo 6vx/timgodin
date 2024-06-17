52f9ed98963575d63e700b6fbea25b1b7b8246fe
Tim Godin <tim_godin@msn.com>
2024-06-17 13:30:58 -0600

    20240617: adding analytics to main?

--- main.ts
+++ main.ts
@@ -10,4 +10,25 @@ import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
 
+import { serve } from 'https://deno.land/std/http/server.ts';
+import { createReporter } from 'https://deno.land/x/g_a/mod.ts';
+
+const ga = createReporter({ id: "G-X4B7FREDZL" }); // Replace with your actual tracking ID
+
+serve((req, connInfo) => {
+  let err;
+  let res;
+  const start = performance.now();
+  try {
+    // Your response handling logic here
+    res = new Response("Hello World");
+  } catch (e) {
+    err = e;
+  } finally {
+    ga(req, connInfo, res, start, err);
+  }
+  return res;
+});
+
+
 await start(manifest, config);
