b313e4bd99870e3bfafdd9c33bf4ae242fcffe30
Tim Godin <tim_godin@msn.com>
2024-07-09 19:19:16 -0600

    20240709: just updating child notes

--- routes/children-of-time.tsx
+++ routes/children-of-time.tsx
@@ -9,7 +9,7 @@ export const handler: Handlers = {
   },
 };
 const data = 
-{"type":"book","isbn-13":"978-1447273288","author":"Adrian Tchaikovsky","title":"Children of Time","description":"Adrian Tchaikovksy's critically acclaimed stand-alone novel Children of Time is the epic story of humanity's battle for survival on a terraformed planet.","published":"20160101","created":"2024-07-04","updated":"2024-07-06","tags":["timgodin"],"private":"false"}
+{"type":"book","isbn-13":"978-1447273288","author":"Adrian Tchaikovsky","title":"Children of Time","description":"Adrian Tchaikovksy's critically acclaimed stand-alone novel Children of Time is the epic story of humanity's battle for survival on a terraformed planet.","published":"20160101","created":"2024-07-04","updated":"2024-07-09","tags":["timgodin"],"private":"false"}
 export default function Home({ data }: PageProps) {
   return (
     <>
@@ -28,10 +28,15 @@ export default function Home({ data }: PageProps) {
 <p><a target="_blank" href="https://www.amazon.com/gp/product/1447273281?ref_=dbs_m_mng_rwt_calw_thcv_0&amp;storetype=ebooks&amp;qid=1720129374&amp;sr=1-1">Amazon</a></p>
 <p>This series won the Hugo for Best Series in 2023, that is how I found it.</p>
 <h2>notes</h2>
-<ul>
-<li>earth has failed</li>
-<li></li>
-</ul>
+<h3>5</h3>
+<p>earth has failed. classic.</p>
+<h3>46</h3>
+<p>Loving this book. At first I thought it was a bit too wackadoodle for me. Like a Neal Stephenson without the ... 'it' factor.</p>
+<p>But I gave it a little more time, and am glad I did. The author may be a good fit for me.</p>
+<p>I was NOT ready to accept spiderworld, but I was drawn in by Porschia on her second gen, and further entrapped by Bianca. I got stuck in the web of a well crafted narrative, what can I say.</p>
+<p>The AI/human hybrid storyline is working for me with Kern. I'm excited to find out where that goes.</p>
+<p>Actually I can't think of one narrative I'm not interested in the resolution to. All the characters have been great.</p>
+<p>I'm glad I didn't give up on it too soon. I usually cut books pretty early.</p>
 
         <br />
         <p class="timestamps">Created: {data.created} - Updated: {data.updated}</p>
